96225e62a9c56049b3dfac31d5c73f4c4628069c
Trisha Moorkoth <tmoo2620@gmail.com>
2025-08-21 22:51:20 -0700

    finished writing solving algorithm

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # sudoku-solver
 Building a sudoku solver using backtracking
+
+given a puzzle, run an algorithm that solves the puzzle
+
+recursive algorithm that solves each cell
+make a choice
+check constraints
+update goal
+
+
+9x9 board
+3 subgrid
+
+make choices and find a globally correct answer (where recursion comes in handy)
+look at both approaches, brute force and backtracking
+
+brute force: generate every single possible board of sudoku, validate each board, return valid board = exponential number of boards
+
+whenver we see a prefilled cell, cant do anyhting
+when we see an empty cell, we do a recursive call to place 1-9
+we will traverse row by row (solve col 0 all the way to col 8 then reset)
+
+constraints: placements cannot break the board
+
+goal reached: fill the whole board (validly), or when u finish the last row and go to the next row = out of bounds
---
+++ main.py
@@ -0,0 +1,61 @@
+# referencing https://www.youtube.com/watch?v=eAFcj_2quWI&ab_channel=Insidecode for brute force solver
+# this program is a sudoku solver which employs backtracking to find the solution of a partially prefilled, valid grid
+
+from collections import defaultdict
+import numpy as np
+
+class Sudoku:
+    def __init__(self):
+        # 2x2 grid
+        self.grid = np.array(ndmin=2)
+        # keys are the row numbers, values are the numbers to prefill as a list
+
+
+'''
+    Args:
+        r - row index
+        c - col index
+        n - number to input in the cell
+    
+    Return:
+        True if n is a valid input and False otherwise
+'''
+def is_valid(grid, r, c, n):
+    # get index of box row and col
+    box_r = r // 3 * 3
+    box_c = c // 3 * 3
+
+    # check if n is unique in its row and col
+    not_in_row = n not in grid[r]
+    not_in_col = n not in [grid[i][c] for i in range(9)]
+
+    # check if n is unique in its box
+    not_in_box = n not in [grid[i][j] for i in range(box_r, box_r + 3) for j in range(box_c, box_c + 3)]
+
+    return not_in_row and not_in_col and not_in_box
+
+'''
+    Recursive method to  fill in values of the grid until a solution is reached
+'''
+def solve(grid, r=0, c=0):
+    # base case and early stop conditions
+    if r == 9:
+        # filled every row, so a valid solution was found
+        return True # maybe return grid?
+    elif c == 9:
+        # reached last column, so start at idx 0 of next row
+        return solve(grid, r + 1, 0)
+    elif grid[r][c] != 0:
+        # if cell is filled alr, move on
+        return solve(grid, r, c + 1)
+    else:
+        # iterate through all possible values of n and check if it's valid
+        for n in range(1,10):
+            if is_valid(r, c, n):
+                # assign n to the cell
+                grid[r][c] = n
+                # recurse based on this state
+                if solve(grid, r, c + 1):
+                    return True
+    return False
+            
